550ef84e67167ae5f37a0c0385f2af83c7d94038
Camilo Farah <camilofarah@gmail.com>
2020-01-04 20:27:36 -0300

    Se ordena el arreglo, sin el uso de threads

--- Tarea2p2.java
+++ Tarea2p2.java
@@ -17,5 +17,20 @@ public class Tarea2p2{
 	      System.out.print(array[x]+" ");
 	    }
 	    System.out.println("\n");
+
+	    //Se ordena el arreglo sin threads
+	    long startTime = System.currentTimeMillis();
+	    MergeSort ms = new MergeSort();
+	    ms.sort(array, 0, array.length-1);
+	    long stopTime = System.currentTimeMillis();
+	    long tiempo_sin_threads = stopTime - startTime;
+
+	    //PRINTEAR ARREGLO ORDENADO SIN THREADS
+	    System.out.println("Arreglo ordenado sin threads:");
+	    for (x = 0; x < array.length; x++){
+	      System.out.print(array[x]+" ");
+	    }
+	    System.out.print("\n");
+	    System.out.println("Tiempo de ejecución para MergeSort sin threads: " + (float)tiempo_sin_threads/1000 + " segundos");
 	}
 }
