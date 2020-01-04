0d8b04bfc797e4f8b6e060fa54ca33e373ed5dd1
Camilo Farah <camilofarah@gmail.com>
2020-01-04 19:52:19 -0300

    Cree un arreglo para que sea ordenado xd buenardo

--- Tarea2p2.java
+++ Tarea2p2.java
@@ -1,1 +1,21 @@
-//xd aiuda
+import java.util.Random;
+
+public class Tarea2p2{
+	public static void main(String[] args){
+
+    	// Se crea un arreglo con valores aleatorios
+	    Random rand = new Random();
+	    int[] array = new int[10];
+	    for (int i=0; i<array.length; i++) {
+	      array[i] = rand.nextInt(100);
+	    }
+
+		//PRINTEAR ARREGLO
+	    System.out.println("Arreglo a ordenar:");
+	    int x;
+	    for (x = 0; x < array.length; x++){
+	      System.out.print(array[x]+" ");
+	    }
+	    System.out.println("\n");
+	}
+}
